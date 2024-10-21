45d27c06aba099ec8fc89a988fb4c54ab1576373
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2024-10-21 23:19:54 +0530

    memory

---
+++ memory.js
@@ -0,0 +1,29 @@
+let My_Youtube_name= "asthatravellingblog.07"
+let another_name= My_Youtube_name
+
+another_name= "owningmyworld"
+
+console.log(another_name);
+console.log(My_Youtube_name);
+console.log(typeof My_Youtube_name);
+
+
+let user_one= {
+    email: "abc@gmail.com",
+    phone_number:1234568899
+};
+
+console.log(user_one.email)
+
+let user_two=user_one
+
+user_two.email="suhani123@gmail.com"
+
+console.log(user_one.email);
+console.log(user_two.email);
+
+
+
+
+
+
